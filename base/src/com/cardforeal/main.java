359ae8dea4173b3d70afb258ea022ca969df12d2
Brett Menzies <scimor5@gmail.com>
2016-03-26 12:50:34 -0700

    Import vrtoolkit

--- base/src/com/cardforeal/main.java
+++ base/src/com/cardforeal/main.java
@@ -1,5 +1,27 @@
 package com.cardforeal;
 
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+import com.google.vrtoolkit.cardboard.audio.CardboardAudioEngine;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.Matrix;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import android.app.Activity;
 import android.os.Bundle;
 
